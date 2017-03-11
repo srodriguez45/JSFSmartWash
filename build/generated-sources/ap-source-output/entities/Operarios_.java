4bea5ebe8fbc03b50f47be3ce0e692f1c5f7e474
srodriguez <stevenrodrod@gmail.com>
2017-03-11 15:57:44 -0500

    actualizacion

--- build/generated-sources/ap-source-output/entities/Operarios_.java
+++ build/generated-sources/ap-source-output/entities/Operarios_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-11T12:52:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-11T15:54:02")
 @StaticMetamodel(Operarios.class)
 public class Operarios_ { 
 
